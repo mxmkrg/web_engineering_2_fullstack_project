6ceab08ea898ecefa2230b6d706ec4207f5d0d4d
Nadiia Shvets <nadiia.shvets@flyeralarm.com>
2025-10-27 14:37:06 +0100

    .

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,14 @@
 import { drizzle } from "drizzle-orm/libsql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in the environment variables.");
+}
 
 export const db = drizzle({
   connection: {
-    // biome-ignore lint/style/noNonNullAssertion: We have a .env file so we expect this to be fine
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 });
